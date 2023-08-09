46a46e0db824d4fb51a00cf93696e1bad0d0239b
FedericoFrau <fedeafrau@gmail.com>
2023-08-09 01:14:38 -0300

    primer commit Trabajo Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Seccion1Component } from './seccion1/seccion1.component';
+import { Seccion2Component } from './seccion2/seccion2.component';
+import { Seccion3Component } from './seccion3/seccion3.component';
+import { Seccion4Component } from './seccion4/seccion4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Seccion1Component,
+    Seccion2Component,
+    Seccion3Component,
+    Seccion4Component
   ],
   imports: [
     BrowserModule
